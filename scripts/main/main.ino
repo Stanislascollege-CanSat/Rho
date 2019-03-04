e10d9372ddc6c95ed70b995399edf254827d2deb
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2019-03-04 13:13:11 +0100

    add pin definitions

--- scripts/main/main.ino
+++ scripts/main/main.ino
@@ -2,6 +2,21 @@
   Rho BabyCan MCU software.
 ****************************/
 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+/******************
+  Pin definitions
+******************/
+
+#define RF_RST 10
+#define RF_CS 12
+#define RF_INT 6
+#define BUZZ 11
+#define A_BAT 9
+
 void setup() {
   // put your setup code here, to run once:
 
