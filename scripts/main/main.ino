02abe69352771cb9f6a3be62af1bc5605cd2fac2
JSSRDRG <39565615+JSSRDRG@users.noreply.github.com>
2019-03-04 09:56:38 +0100

    update

---
+++ scripts/main/main.ino
@@ -0,0 +1,13 @@
+/****************************
+  Rho BabyCan MCU software.
+****************************/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
